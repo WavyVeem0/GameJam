053062a679b2a1fbd32f40ce9060f78e1dce5b8b
Sined <deniska6_2005@mail.ru>
2023-11-11 14:58:07 +0300

    Shake Script

---
+++ Jam/Assets/Shake.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public float ShakeScale = 1;
+    private Vector3 startPosition;
+    private void Awake()
+    {
+        startPosition = transform.position;
+        StateOfShake(true);
+    }
+
+    public void StateOfShake(bool shaking)
+    {
+        if (shaking) StartCoroutine(Shaking());
+        else StopAllCoroutines();
+    }
+    private IEnumerator Shaking()
+    {
+        for(; ; )
+        {
+            float x = Random.Range(0, ShakeScale);
+            float y = Random.Range(0, ShakeScale);
+            transform.position = new Vector3(startPosition.x + x, startPosition.y + y, startPosition.z);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
