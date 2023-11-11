830e2f17624d83ad84af7d82aa22c85c856de0a6
WavyVeemo <kolya12911@gmail.com>
2023-11-11 16:39:29 +0300

    Using first inventory item

--- Jam/Assets/Move.cs
+++ Jam/Assets/Move.cs
@@ -19,7 +19,7 @@ public class Move : MonoBehaviour
         float axisY = Input.GetAxis("Vertical");
         body.velocity = new Vector2(axisX * speed,axisY * speed);
         Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; 
-        float rotateZ = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg - 80; 
+        float rotateZ = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg - 90; 
         transform.rotation=Quaternion.Euler(0f,0f,rotateZ);
     }
 }
